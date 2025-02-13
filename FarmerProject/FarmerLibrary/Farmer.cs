d31475e09d3e223236d519f006bb22a3248af707
Дмитрий Лёвкин <jonikm892@gmail.com>
2025-02-13 13:22:41 +0300

    WIP

---
+++ FarmerProject/FarmerLibrary/Farmer.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FarmerLibrary
+{
+
+
+    /// <summary>
+    /// Класс Farmer в котором находятся методы для нахождения площади зная лишь периметр
+    /// </summary>
+    public static class Farmer
+    {
+
+        // Метод вычисления площади квадрата
+        public static double SquareArea(double perimeter)
+        {
+            try
+            {
+                if (perimeter < 0)
+                {
+                    Console.WriteLine("Периметр не может быть отрицательным");
+                    return 0;
+
+                }
+                else
+                {
+                    double S = Math.Pow((perimeter / 4), 2);
+                    return S;
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+        }
+        // Метод вычисления площади равностороннего треугольника
+        public static double EquilateralTriangleArea(double perimeter)
+        {
+            try
+            {
+                if (perimeter < 0)
+                {
+                    Console.WriteLine("Периметр не может быть отрицательным");
+                    return 0;
+                }
+                else
+                {
+                    double side = perimeter / 3;
+                    return (Math.Sqrt(3) / 4) * Math.Pow(side, 2);
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+        }
+
+        // Метод вычисления площади правильного ромба (предполагается, что он состоит из двух равносторонних треугольников)
+        public static double RhombusArea(double perimeter)
+        {
+            try
+            {
+                if (perimeter < 0)
+                {
+                    Console.WriteLine("Периметр не может быть отрицательным");
+                    return 0;
+                }
+                else
+                {
+                    double side = perimeter / 4;
+                    return 2 * ((Math.Sqrt(3) / 4) * Math.Pow(side, 2));
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+        }
+
+        // Метод вычисления площади правильного пятиугольника
+        public static double PentagonArea(double perimeter)
+        {
+            try
+            {
+                if (perimeter < 0)
+                {
+                    Console.WriteLine("Периметр не может быть отрицательным");
+                    return 0;
+                }
+                else
+                {
+                    double side = perimeter / 5;
+                    return (5 * Math.Pow(side, 2)) / (4 * Math.Tan(Math.PI / 5));
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+        }
+
+        // Метод вычисления площади прямоугольника, у которого одна сторона вдвое больше другой
+        public static double RectangleArea(double perimeter)
+        {
+            try
+            {
+                if (perimeter < 0)
+                {
+                    Console.WriteLine("Периметр не может быть отрицательным");
+                    return 0;
+                }
+                else
+                {
+                    double shortSide = perimeter / 6;
+                    double longSide = 2 * shortSide;
+                    return shortSide * longSide;
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+        }
+
+        // Метод вычисления площади круга
+        public static double CircleArea(double perimeter)
+        {
+            try
+            {
+                if (perimeter < 0)
+                {
+                    Console.WriteLine("Периметр не может быть отрицательным");
+                    return 0;
+                }
+                else
+                {
+                    double radius = perimeter / (2 * Math.PI);
+                    return Math.PI * Math.Pow(radius, 2);
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+        }
+
+        // Метод вычисления площади правильного шестиугольника
+        public static double HexagonArea(double perimeter)
+        {
+            try
+            {
+                if (perimeter < 0)
+                {
+                    Console.WriteLine("Периметр не может быть отрицательным");
+                    return 0;
+                }
+                else
+                {
+                    double side = perimeter / 6;
+                    return (3 * Math.Sqrt(3) / 2) * Math.Pow(side, 2);
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+        }
+    }
+}
---
+++ FarmerProject/FarmerTest/Program.cs
@@ -0,0 +1,78 @@
+﻿using FarmerLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FarmerTest
+{
+    public class Program
+    {
+        public static void Test1() // Тест площади круга CircleArea
+        {
+            double p = 9.23;
+            double s = Farmer.CircleArea(p);
+            Console.WriteLine("Площадь круга: " + s);
+        }
+
+        static void Test2() // Тест площади равностороннего треугольника EquilateralTriangleArea
+        {
+            double p = -9.5;
+            double s = Farmer.EquilateralTriangleArea(p);
+            Console.WriteLine("Площадь равностороннего треугольника: " + s);    
+        }
+
+        static void Test3() // Тест площади квадрата SquareArea
+        {
+            double p = -36;
+            double s = Farmer.SquareArea(p);
+            Console.WriteLine("Площадь квадрата: " + s);
+        }
+
+        static void Test4() // Тест площади правильного ромба RhombusArea
+        {
+            double p = 315;
+            double s = Farmer.RhombusArea(p);
+            Console.WriteLine("Площадь правильного ромба: " + s);
+        }
+        
+        static void Test5() // Тест площади правильного пятиугольника PentagonArea
+        {
+            double p = 55;
+            double s = Farmer.PentagonArea(p);
+            Console.WriteLine("Площадь правильного пятиугольника: " + s);
+        }
+
+        static void Test6() // Тест площади прмоугольника, у которого одна сторона вдвое больше другой RectangleArea
+        {
+            double p = 5.600;
+            double s = Farmer.RectangleArea(p);
+            Console.WriteLine("Площадь прямоугольника, у которого одна сторона вдвое больше другой: " + s);
+        }
+
+        static void Test7() // Тест площади правильного шестиугольника HexagonArea
+        {
+            double p = 0.26;
+            double s = Farmer.HexagonArea(p);
+            Console.WriteLine("Площадь правильного шестиугольника: " + s);
+        }
+
+        /// <summary>
+        /// Тесты и вывод их результатов
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Test1();
+            Test2();
+            Test3();
+            Test4();
+            Test5();
+            Test6();
+            Test7();
+            Console.ReadKey();
+        }
+    }
+}
